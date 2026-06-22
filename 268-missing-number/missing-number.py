23ae24734c0cdd48d4b5135bcc373f644f24c135
mat2127 <matiyosgashaw3@gmail.com>
2026-06-22 10:48:18 +0300

    Time: 1393 ms (10.13%) | Memory: 20.4 MB (74.94%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -3,8 +3,7 @@ class Solution:
         for i in range(len(nums)+1):
             if i not in nums:
                 return i
-            else:
-                continue
+            
 
    
         
